40d2f78c06c048101e5feeea54877f3b7d1881b4
xlanex6 <alexduval71@gmail.com>
2024-04-15 14:23:06 +0200

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -33,39 +33,43 @@ const posts = ref([])
 
 async function fetchPosts() {
   const res = await fetch(`https://dummyjson.com/posts?limit=${postCounter.value}`)
-  const { posts: postData, skip , limit } = await res.json()
+  const { posts: postData, skip, limit } = await res.json()
   posts.value = postData
 }
 
 </script>
 
 <template>
+  <div class="bg-gray-100 min-h-screen p-4 m-auto max-w-md">
 
-  <!-- <p v-if="count >= 10" class="winner">GAGNÉ</p>
-    <p v-else class="looser">PERDU</p>
-    
-    <button @click="decrementCount()"> - </button>
-    Nombre de clicks: {{ count }}
-    <button @click="incrementCount()"> + </button>
-    
-    <hr> -->
 
-  <!-- <button @click.once="addVanilla()"> Add vanilla </button> -->
-  <!-- <input type="text" v-model="userInput">
-  <button @click="addUserInput"> Add </button>
-  <ul>
-    <li v-for="lib in libs" :key="lib"> {{ lib }}</li>
-  </ul> -->
+    <!-- <p v-if="count >= 10" class="winner">GAGNÉ</p>
+      <p v-else class="looser">PERDU</p>
+      
+      <button @click="decrementCount()"> - </button>
+      Nombre de clicks: {{ count }}
+      <button @click="incrementCount()"> + </button>
+      
+      <hr> -->
 
-  <input type="number" :max="maxPosts" min="0" v-model="postCounter">
+    <!-- <button @click.once="addVanilla()"> Add vanilla </button> -->
+    <!-- <input type="text" v-model="userInput">
+        <button @click="addUserInput"> Add </button>
+        <ul>
+          <li v-for="lib in libs" :key="lib"> {{ lib }}</li>
+        </ul> -->
 
-  <button @click="fetchPosts" v-bind:disabled="postCounter > maxPosts + 1"> Get {{ postCounter }} Posts </button>
+    <input type="number" :max="maxPosts" min="0" v-model="postCounter">
 
-  <article v-for="post in posts" :key="post.id">
-    <h3>{{ post.title }}</h3>
-    <p>{{ post.body }}</p>
-  </article>
+    <button @click="fetchPosts" class="bg-red-500 rounded-xl px-4 py-1 font-bold shadow-lg text-white mx-2" v-bind:disabled="postCounter > maxPosts + 1"> Get {{ postCounter }} Posts </button>
 
+    <article v-for="post in posts" :key="post.id" class="mb-8">
+      <h3 class="text-xl font-bold">{{ post.title }}</h3>
+      <p class="text-sm mb-2">{{ post.body }}</p>
+      <hr>
+    </article>
+
+  </div>
 </template>
 
 <style scoped>
